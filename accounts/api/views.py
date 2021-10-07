409ab61ed26b01adf3a92bdf10d7b4c62dd0304a
YanliuWang <wyl199825@gmail.com>
2021-10-06 21:33:19 -0700

    add anotations

--- accounts/api/views.py
+++ accounts/api/views.py
@@ -11,4 +11,5 @@ class UserViewSet(viewsets.ReadOnlyModelViewSet):
     """
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
+    # 每一次操作都必须进行登录检测
     permission_classes = [permissions.IsAuthenticated]
